f53bf7bc1b769411e421ce0f964c18f5837bce7d
pandeyraj333 <139428980+pandeyraj333@users.noreply.github.com>
2025-07-17 23:37:06 +0530

    Update streamlit_app.py
    details fetch

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,10 +28,66 @@ def fetch_property_features(property_id):
         dataframe = pd.DataFrame(rows)
     return dataframe
 
+def fetch_property_details(property_id):
+    url = f"https://ap-southeast-2.api.vaultre.com.au/api/v1.3/properties/{property_id}/features"
+
+    headers = {
+        'Authorization': st.secrets["api"]["auth_token"],
+        'x-api-key': st.secrets["api"]["api_key"]
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        rows = []
+        rows.append({
+            'displayAddress': data['displayAddress'],
+            'saleLifeId': data['saleLifeId'],
+            'bed': data['bed'],
+            'bath': data['bath'],
+            'garages': data['garages'],
+            'carports': data['carports'],
+            'openSpaces': data['openSpaces'],
+            'ensuites': data['ensuites'],
+            'toilets': data['toilets'],
+            'floorArea': data['floorArea']['value'],
+            'landArea': data['landArea']['value'],
+            'receptionRooms': data['receptionRooms'],
+            'yearBuilt': data['yearBuilt'],
+            'lotNumber': data['lotNumber'],
+            'rpdp': data['rpdp'],
+            'certificateOfTitle': data['certificateOfTitle'],
+            'legalDescription': data['legalDescription'],
+            'landValue': data['landValue'],
+            'improvementValue': data['improvementValue'],
+            'rateableValue': data['rateableValue'],
+            'Council':  data['rates']['council']['value'],
+            'methodOfSale': data['methodOfSale'],
+            'heading': data['heading'],
+            'description': data['description'],
+            'internalRemarks': data['internalRemarks'],
+            'editableBy':  data['editableBy'][0]['name']
+        })
+        
+        df = pd.DataFrame(rows)
+    return df
+
+
 # Streamlit UI
 st.title(f"🏡 Property Feature Lookup token:{st.secrets["api"]["auth_token"]} Key: {st.secrets["api"]["api_key"]}")
 
 property_id = st.text_input("Enter Property ID", placeholder="e.g., 30029515")
+
+if st.button("Get Details"):
+    if property_id:
+        df = fetch_property_details(property_id.strip())
+        if df is not None and not df.empty:
+            st.dataframe(df, use_container_width=True)
+        else:
+            st.warning("No Details found or invalid property ID.")
+    else:
+        st.warning("Please enter a Property ID.")
 
 if st.button("Get Features"):
     if property_id:
